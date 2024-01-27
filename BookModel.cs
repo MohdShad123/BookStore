1df89277358cac30da40faa9570377bcab2036ba
MohdShad123 <89112962+MohdShad123@users.noreply.github.com>
2024-01-27 23:54:01 +0530

    Update BookModel.cs

--- BookModel.cs
+++ BookModel.cs
@@ -1,9 +1,11 @@
 namespace BookStore.Models
 {
-	public class BookModel
-	{
+    public class BookModel
+    {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+
+        public string Description { get; set; }
     }
 }
