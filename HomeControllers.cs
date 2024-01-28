2f3e8133c14327594a0367e43103753b9d676cd4
MohdShad123 <89112962+MohdShad123@users.noreply.github.com>
2024-01-29 01:38:49 +0530

    Update HomeControllers.cs

--- HomeControllers.cs
+++ HomeControllers.cs
@@ -1,22 +1,27 @@
+using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
 
 namespace BookStore.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ViewResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewData["property1"] = "Mohd Usmani";
 
-        	public ViewResult AboutUs()
-        	{
-            		return View();
-        	}
+            ViewData["book"] = new BookModel() { Author = "Nagarro", Id = 1 };
+            return View();
+        }
 
-		public ViewResult ContactUs()
-		{
-			return View();
-		}
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
     }
 }
