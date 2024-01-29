fabc60efbc9f08e9a79493edcd99423c61a324b3
MohdShad123 <89112962+MohdShad123@users.noreply.github.com>
2024-01-29 13:24:07 +0530

    Update BookController.cs

--- BookController.cs
+++ BookController.cs
@@ -1,39 +1,39 @@
 using BookStore.Models;
 using BookStore.Repository;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Threading;
 
 namespace BookStore.Controllers
 {
-	public class BookController : Controller
-	{
-		private readonly BookRepository bookRepository = null;
+    public class BookController : Controller
+    {
+        private readonly BookRepository bookRepository = null;
 
-		public BookController()
+        public BookController()
         {
-			bookRepository = new BookRepository();
-		}
+            bookRepository = new BookRepository();
+        }
         public ViewResult GetAllBooks()
-		{
-			//return "All books";
-			var data =  bookRepository.GetAllBooks();
-			return View();
-		}
+        {
+            //return "All books";
+           var data =  bookRepository.GetAllBooks();
+           return View(data);
+        }
 
-		public ViewResult GetBook(int id)
-		{
-			//return $"book with id = {id}";
-			var data =  bookRepository.GetBookById(id);
-			return View();
-		}
+        [Route("book-details/{id}")]
+        public ViewResult GetBook(int id)
+        {
+            //return $"book with id = {id}";
+
+            var data =  bookRepository.GetBookById(id);
+            return View(data);
+        }
 
 
-		//http://localhost:43862/book/searchbooks?bookName=MVCBooks&authorName=Nagarro
-		public ViewResult SearchBooks(string bookName, string authorName)
-		{
-            var data = bookRepository.SearchBook(bookName, authorName);
-            return View();
-         }
-	}
+        //http://localhost:43862/book/searchbooks?bookName=MVCBooks&authorName=Nagarro
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            //return $"Book with name = {bookName} & Author = {authorName}";
+            return bookRepository.SearchBook(bookName, authorName);
+        }
+    }
 }
