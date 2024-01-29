469a25b911b113df8e46b0ecf1f0541a4b3c8a49
MohdShad123 <89112962+MohdShad123@users.noreply.github.com>
2024-01-29 13:27:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+#if DEBUG
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+#endif
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "bookApp/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
